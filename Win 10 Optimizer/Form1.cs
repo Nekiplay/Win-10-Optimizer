72b9d4154e6f3ba500a83e2bf36e36c6c41da87a
Neki_play <neci.03@mail.ru>
2021-02-27 07:43:19 +0500

    Fix 3

--- Win 10 Optimizer/Form1.cs
+++ Win 10 Optimizer/Form1.cs
@@ -88,9 +88,6 @@ namespace Win_10_Optimizer
             public static Color color1 = Color.FromArgb(172, 126, 241);
             public static Color color2 = Color.FromArgb(249, 118, 176);
             public static Color color3 = Color.FromArgb(253, 138, 114);
-            public static Color color4 = Color.FromArgb(95, 77, 221);
-            public static Color color5 = Color.FromArgb(249, 88, 155);
-            public static Color color6 = Color.FromArgb(24, 161, 251);
         }
 
         private void panel1_MouseDown(object sender, MouseEventArgs e)
