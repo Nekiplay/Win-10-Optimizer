bcb281c1cd40644fbf0ae4eedfc38860484a642c
Neki_play <neci.03@mail.ru>
2021-03-09 22:52:43 +0500

    Code optimize

--- Win 10 Optimizer/Forms/EnergyOptimize/EnergyClass.cs
+++ Win 10 Optimizer/Forms/EnergyOptimize/EnergyClass.cs
@@ -18,53 +18,64 @@ namespace Win_10_Optimizer.Forms.EnergyOptimize
             foreach (string text in cmdtext)
             {
                 string text1 = text;
-                if (!string.IsNullOrEmpty(text1))
+                if (!string.IsNullOrEmpty(GetSchemeID(text1)))
                 {
-                    if (text1.Contains("GUID"))
+                    Console.WriteLine(text1);
+                    text1 = text1.Replace(" (", "&");
+                    text1 = text1.Replace(")", "&");
+                    string type = Regex.Match(text1, "&(.*)&").Groups[1].Value;
+                    string id = GetSchemeID(text1);
+                    if (text1.Contains("*"))
                     {
-                        text1 = text1.Replace(" (", "&");
-                        text1 = text1.Replace(")", "&");
-                        string type = Regex.Match(text1, "&(.*)&").Groups[1].Value;
-                        string id = Regex.Match(text1, "GUID схемы питания: (.*) &").Groups[1].Value;
-                        if (text1.Contains("*"))
-                        {
-                            list.Add(new Tuple<string, string, bool>(id, type, true));
-                        }
-                        else
-                        {
-                            list.Add(new Tuple<string, string, bool>(id, type, false));
-                        }
+                        list.Add(new Tuple<string, string, bool>(id, type, true));
+                    }
+                    else
+                    {
+                        list.Add(new Tuple<string, string, bool>(id, type, false));
                     }
                 }
             }
             return list;
         }
-        public string CreateMaximum()
+        private string GetSchemeID(List<string> cmdtext)
         {
-            List<string> cmdtext = new Win_10_Optimizer.Utilites.ProcessUtils().StartCmd("chcp 1251 & powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61");
+            string id = "";
             foreach (string text in cmdtext)
             {
-                string text1 = text;
-                if (!string.IsNullOrEmpty(text1))
+                if (string.IsNullOrEmpty(id))
                 {
-                    if (text1.Contains("GUID"))
-                    {
-                        text1 = text1.Replace(" (", "&");
-                        text1 = text1.Replace(")", "&");
-                        string id = Regex.Match(text1, "GUID схемы питания: (.*) &").Groups[1].Value;
-                        return id;
-                    }
+                    id = GetSchemeID(text);
+                }
+            }
+            return id;
+        }
+        private string GetSchemeID(string text)
+        {
+            string text1 = text;
+            if (!string.IsNullOrEmpty(text1))
+            {
+                if (text1.Contains("GUID"))
+                {
+                    text1 = text1.Replace(" (", "&");
+                    text1 = text1.Replace(")", "&");
+                    string id = Regex.Match(text1, "GUID схемы питания: (.*) &").Groups[1].Value;
+                    return id;
                 }
             }
             return "";
         }
+        public string CreateMaximum()
+        {
+            List<string> cmdtext = new Win_10_Optimizer.Utilites.ProcessUtils().StartCmd("chcp 1251 & powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61");
+            return GetSchemeID(cmdtext);
+        }
         public void SetActiv(string id)
         {
-            new Win_10_Optimizer.Utilites.ProcessUtils().StartCmd("chcp 1251 & powercfg -SETACTIVE " + id);
+            List<string> cmdtext = new Win_10_Optimizer.Utilites.ProcessUtils().StartCmd("chcp 1251 & powercfg -SETACTIVE " + id);
         }
         public void Delete(string id)
         {
-            new Win_10_Optimizer.Utilites.ProcessUtils().StartCmd("chcp 1251 & powercfg /d " + id);
+            List<string> cmdtext = new Win_10_Optimizer.Utilites.ProcessUtils().StartCmd("chcp 1251 & powercfg /d " + id);
         }
         public void Enable(bool on)
         {
