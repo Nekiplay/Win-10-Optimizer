2b950cfc628312d5badfe91c306ec38d1144be7a
Neki play <Herobrine858@outlook.com>
2021-03-16 07:32:27 +0500

    Fixes

--- Win 10 Optimizer/Forms/Cleaner/CleanerFileSettings.cs
+++ Win 10 Optimizer/Forms/Cleaner/CleanerFileSettings.cs
@@ -102,7 +102,6 @@ namespace Win_10_Optimizer
             new ClearFiles(Environment.GetFolderPath(Environment.SpecialFolder.Windows) + "\\Temp", "*.*", true),
             new ClearFiles(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\AppData\\Local\\Temp", "*.*", true),
             new ClearFiles(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\AppData\\Local\\Microsoft\\Windows\\History", "*.*", true),
-            new ClearFiles("C:\\aoshax", "*.*", true),
             new ClearFiles("C:\\Windows\\Prefetch", "*.pf", true),
             new ClearFiles("C:\\Users\\All Users\\Microsoft\\Windows\\WER\\ReportQueue", "*.*", true),
             new ClearFiles("C:\\ProgramData\\Microsoft\\Windows\\WER\\ReportQueue", "*.*", true),
